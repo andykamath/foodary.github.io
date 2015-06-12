88e01f37831aa8e333a2a8b46e121d955666d803
Andy Kamath <andy@andykamath.net>
2015-06-12 10:48:47 -0400

    Update action_answer_placelist.php

--- action_answer_placelist.php
+++ action_answer_placelist.php
@@ -30,8 +30,14 @@
                     $location = $result['geometry']['location']['lat'].",".$result['geometry']['location']['lng'];
                     $spacename = str_replace(" ", "+", $result['name']);
                     $spaceaddress = str_replace(" ", "+", $result['vicinity']);
+                    $location = explode(",", $_GET['action']);
+                    $lat1 = floatval($location[0]);
+                    $long1 = floatval($location[1]);
+                    $lat2 = floatval($result['geometry']['location']['lat']);
+                    $long2 = floatval($result['geometry']['location']['lng']);
                     $loc = "http://www.google.com/maps/dir/".$_GET['action']."/".$spacename.",+".$spaceaddress;
-    			    echo "<img src=\"https://maps.googleapis.com/maps/api/place/photo?key=AIzaSyDsBsq9Dei-OnXAYzrSZHC2s6Mvx8_XCxo&photoreference=".$r['photo_reference']."&maxheight=200\"><br>".$result['name']."`".$result['vicinity']."ccc".$loc.";";	
+    			    echo "<img src=\"https://maps.googleapis.com/maps/api/place/photo?key=AIzaSyDsBsq9Dei-OnXAYzrSZHC2s6Mvx8_XCxo&photoreference=".$r['photo_reference']."&maxheight=200\"><br>".$result['name']." ".distance($lat1,$long1,$lat2,$long2)."`".$result['vicinity']."ccc".$loc.";";	
+                    echo $result['name']."ccc".$result['']
                 }
     		}
     	}
@@ -41,8 +47,25 @@
             $spacename = str_replace(" ", "+", $result['name']);
             $spaceaddress = str_replace(" ", "+", $result['vicinity']);
             $loc = "http://www.google.com/maps/dir/".$_GET['action']."/".$spacename.",+".$spaceaddress;
-			echo "<img src=\"".$result['icon']."\" height=\"200\"><br>".$result['name']."`".$result['vicinity']."<br>Rating: ".$result['rating']."/5ccc".$loc.";";
+            $dist = distance()
+			echo "<img src=\"".$result['icon']."\" height=\"200\"><br>".$result['name']."`".$result['vicinity']."ccc".$loc.";";
     	}
 	}
+    function distance($lat1, $lon1, $lat2, $lon2, $unit) {
+  $theta = $lon1 - $lon2;
+  $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+  $dist = acos($dist);
+  $dist = rad2deg($dist);
+  $miles = $dist * 60 * 1.1515;
+  $unit = strtoupper($unit);
+  if ($unit == "K") {
+    return ($miles * 1.609344);
+  } else if ($unit == "N") {
+      return ($miles * 0.8684);
+    } else {
+        return $miles;
+      }
+}
+
 ?>
 35.109052,-80.748268
