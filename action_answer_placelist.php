f8de27fbb406a09faf7f226981fae348a59f4e09
Andy Kamath <andy@andykamath.net>
2015-06-15 15:48:07 -0400

    Update action_answer_placelist.php

--- action_answer_placelist.php
+++ action_answer_placelist.php
@@ -1,10 +1,6 @@
 <?php
-	header("Access-Control-Allow-Origin: *");
-	header("Access-Control-Allow-Credentials: true");
-    	header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-    	header('Access-Control-Max-Age: 1000');
-    	header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
-	$res = file_get_contents("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=".$_GET['action']."&radius=5000&types=food&key=AIzaSyAZwbQCDTf-AKn0dvdNahrgXOqkZQTydTQ");
+    	header("Access-Control-Allow-Origin: *");
+	$res = file_get_contents("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=".$_POST['action']."&radius=5000&types=food&key=AIzaSyAZwbQCDTf-AKn0dvdNahrgXOqkZQTydTQ");
 	$jsonContent = json_decode($res, true);
 	foreach ($jsonContent['results'] as $result) {
     	// now you have the $result array that contains the location of the place
@@ -18,7 +14,7 @@
                     $location = $result['geometry']['location']['lat'].",".$result['geometry']['location']['lng'];
                     $spacename = str_replace(" ", "+", $result['name']);
                     $spaceaddress = str_replace(" ", "+", $result['vicinity']);
-                    $loc = "http://www.google.com/maps/dir/".$_GET['action']."/".$spacename.",+".$spaceaddress;
+                    $loc = "http://www.google.com/maps/dir/".$_POST['action']."/".$spacename.",+".$spaceaddress;
     			    echo "<img src=\"https://maps.googleapis.com/maps/api/place/photo?key=AIzaSyDsBsq9Dei-OnXAYzrSZHC2s6Mvx8_XCxo&photoreference=".$r['photo_reference']."&maxheight=200\"><br>".$result['name']."`".$result['vicinity']."ccc".$loc.";";	
                 }
     		}
@@ -28,7 +24,7 @@
             $location = $result['geometry']['location']['lat'].",".$result['geometry']['location']['lng'];
             $spacename = str_replace(" ", "+", $result['name']);
             $spaceaddress = str_replace(" ", "+", $result['vicinity']);
-            $loc = "http://www.google.com/maps/dir/".$_GET['action']."/".$spacename.",+".$spaceaddress;
+            $loc = "http://www.google.com/maps/dir/".$_POST['action']."/".$spacename.",+".$spaceaddress;
             if($result['opening_hours']['open_now'])
             {
             	$open = "Open at this time";
