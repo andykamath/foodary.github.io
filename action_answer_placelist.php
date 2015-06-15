b834a5d0c9b900edd45a9d0d206f250bdd2f1072
Andy Kamath <andy@andykamath.net>
2015-06-15 16:03:45 -0400

    Update action_answer_placelist.php

--- action_answer_placelist.php
+++ action_answer_placelist.php
@@ -1,7 +1,23 @@
 <?php
-    	header("Access-Control-Allow-Origin: *");
-    	header("Access-Control-Allow-Methods: GET,PUT,POST,DELETE");
-    	header("Access-Control-Allow-Headers: Content-Type");
+    	// Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+        exit(0);
+    }
+
 	$res = file_get_contents("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=".$_POST['action']."&radius=5000&types=food&key=AIzaSyAZwbQCDTf-AKn0dvdNahrgXOqkZQTydTQ");
 	$jsonContent = json_decode($res, true);
 	foreach ($jsonContent['results'] as $result) {
